61ce126bab177eb81a7ca61ab410715bf5097e92
LordGedelicious <34541559+LordGedelicious@users.noreply.github.com>
2023-05-11 18:20:38 +0700

    created dummy data and real podes data

---
+++ model/dummy_sni.py
@@ -0,0 +1,96 @@
+import numpy as np
+import pandas as pd
+import random
+
+# Total feature combinations:
+# = (5 x 2) + (7 x 2) + (6 x 2) + (3 x 2 x 4 x 5 x 2 x 2 x 4)
+# = 36960
+total_rows = 50000
+
+row_data = []
+
+for row_id in range(total_rows):
+    # Create row info about "tangki septik"
+    tangkiSeptik_penggunaan = np.random.choice(["Berfungsi","Tidak Berfungsi"])
+    tangkiSeptik_penggunaan_num = 1 if tangkiSeptik_penggunaan == "Berfungsi" else 0
+    tangkiSeptik_terawat = np.random.choice(["Ya","Tidak"])
+    tangkiSeptik_terawat_num = 1 if tangkiSeptik_terawat == "Ya" else 0
+    tangkiSeptik_bauTidakSedap = np.random.choice(["Ya","Tidak"])
+    tangkiSeptik_bauTidakSedap_num = 0 if tangkiSeptik_bauTidakSedap == "Ya" else 1
+    tangkiSeptik_ketercemaranLingkungan = np.random.choice(["Ya","Tidak"])
+    tangkiSeptik_ketercemaranLingkungan_num = 0 if tangkiSeptik_ketercemaranLingkungan == "Ya" else 1
+    tangkiSeptik_jarakDariSumberAir = round(random.uniform(0.0, 50.0), 2)
+    tangkiSeptik_jarakDariSumberAir_num = 1 if tangkiSeptik_jarakDariSumberAir >= 15 else 0
+    tangkiSeptik_overallScore = 0.3 * tangkiSeptik_penggunaan_num + 0.3 * tangkiSeptik_terawat_num + 0.05 * tangkiSeptik_bauTidakSedap_num + 0.15 * tangkiSeptik_ketercemaranLingkungan_num + 0.2 * tangkiSeptik_jarakDariSumberAir_num
+    
+    # Create row info about "sarana pembuangan BAB"
+    saranaPembuanganBAB_penggunaan = np.random.choice(["Berfungsi","Tidak Berfungsi"])
+    saranaPembuanganBAB_penggunaan_num = 1 if saranaPembuanganBAB_penggunaan == "Berfungsi" else 0
+    saranaPembuanganBAB_bangunanTerawat = np.random.choice(["Ya","Tidak"])
+    saranaPembuanganBAB_bangunanTerawat_num = 1 if saranaPembuanganBAB_bangunanTerawat == "Ya" else 0
+    saranaPembuanganBAB_adaDindingBangunan = np.random.choice(["Ya","Tidak"])
+    saranaPembuanganBAB_adaDindingBangunan_num = 1 if saranaPembuanganBAB_adaDindingBangunan == "Ya" else 0
+    saranaPembuanganBAB_adaVentilasiPintu = np.random.choice(["Ya","Tidak"])
+    saranaPembuanganBAB_adaVentilasiPintu_num = 1 if saranaPembuanganBAB_adaVentilasiPintu == "Ya" else 0
+    saranaPembuanganBAB_peneranganCukup = np.random.choice(["Ya","Tidak"])
+    saranaPembuanganBAB_peneranganCukup_num = 1 if saranaPembuanganBAB_peneranganCukup == "Ya" else 0
+    saranaPembuanganBAB_klosetLeherAngsa = np.random.choice(["Ya","Tidak"])
+    saranaPembuanganBAB_klosetLeherAngsa_num = 1 if saranaPembuanganBAB_klosetLeherAngsa == "Ya" else 0
+    saranaPembuanganBAB_adaAirBersih = np.random.choice(["Ya","Tidak"])
+    saranaPembuanganBAB_adaAirBersih_num = 1 if saranaPembuanganBAB_adaAirBersih == "Ya" else 0
+    saranaPembuanganBAB_overallScore = 0.25 * saranaPembuanganBAB_penggunaan_num + 0.1 * saranaPembuanganBAB_bangunanTerawat_num + 0.1 * saranaPembuanganBAB_adaDindingBangunan_num + 0.05 * saranaPembuanganBAB_adaVentilasiPintu_num + 0.05 * saranaPembuanganBAB_peneranganCukup_num + 0.2 * saranaPembuanganBAB_klosetLeherAngsa_num + 0.25 * saranaPembuanganBAB_adaAirBersih_num
+    
+    # Create row info about "IPLT"
+    iplt_penggunaan = np.random.choice(["Berfungsi","Tidak Berfungsi"])
+    iplt_penggunaan_num = 1 if iplt_penggunaan == "Berfungsi" else 0
+    iplt_bangunanTerawat = np.random.choice(["Ya","Tidak"])
+    iplt_bangunanTerawat_num = 1 if iplt_bangunanTerawat == "Ya" else 0
+    iplt_bauTidakSedap = np.random.choice(["Ya","Tidak"])
+    iplt_bauTidakSedap_num = 0 if iplt_bauTidakSedap == "Ya" else 1
+    iplt_pencemaranLingkungan = np.random.choice(["Ya","Tidak"])
+    iplt_pencemaranLingkungan_num = 0 if iplt_pencemaranLingkungan == "Ya" else 1
+    iplt_airLimbahBerwarna = np.random.choice(["Ya","Tidak"])
+    iplt_airLimbahBerwarna_num = 0 if iplt_airLimbahBerwarna == "Ya" else 1
+    iplt_jarakDariPermukiman = round(random.uniform(0.0, 20.0), 2)
+    iplt_jarakDariPermukiman_num = 1 if iplt_jarakDariPermukiman >= 5 else 0
+    iplt_overallScore = 0.25 * iplt_penggunaan_num + 0.2 * iplt_bangunanTerawat_num + 0.05 * iplt_bauTidakSedap_num + 0.15 * iplt_pencemaranLingkungan_num + 0.15 * iplt_airLimbahBerwarna_num + 0.2 * iplt_jarakDariPermukiman_num
+    
+    # Create row info about "saluran drainase"
+    saluranDrainase_hierarki = np.random.choice(["Primer","Sekunder","Tersier"])
+    saluranDrainase_hierarki_num = 0.05
+    saluranDrainase_jenis = np.random.choice(["Terbuka","Tertutup"])
+    saluranDrainase_jenis_num = 0.1
+    saluranDrainase_bentukPenampang = np.random.choice(["Persegi", "Trapesiun", "Setengah Lingkaran", "Segitiga"])
+    saluranDrainase_bentukPenampang_num = 0.05
+    saluranDrainase_perkerasanTepi = np.random.choice(["Pasir Halus","Lempung Kepasiran","Tanah","Kerikil Halus","Batuan"])
+    saluranDrainase_perkerasanTepi_num = 0.1 if saluranDrainase_perkerasanTepi in ["Kerikil Halus", "Batuan"] else 0
+    saluranDrainase_kondisiDrainase = np.random.choice(["Terawat","Tidak Terawat"])
+    saluranDrainase_kondisiDrainase_num = 0.3 if saluranDrainase_kondisiDrainase == "Terawat" else 0
+    saluranDrainase_bauTidakSedap = np.random.choice(["Ya","Tidak"])
+    saluranDrainase_bauTidakSedap_num = 0 if saluranDrainase_bauTidakSedap == "Ya" else 0.1
+    saluranDrainase_sedimentasiDrainase = np.random.choice(["Tidak Ada","Tanah","Sampah","Eceng Gondok"])
+    saluranDrainase_sedimentasiDrainase_num = 0.3 if saluranDrainase_sedimentasiDrainase == "Tidak Ada" else 0
+    saluranDrainase_overallScore = saluranDrainase_hierarki_num + saluranDrainase_jenis_num + saluranDrainase_bentukPenampang_num + saluranDrainase_perkerasanTepi_num + saluranDrainase_kondisiDrainase_num + saluranDrainase_bauTidakSedap_num + saluranDrainase_sedimentasiDrainase_num
+    
+    # Check results for classification process
+    tangkiSeptik_class = "0" if tangkiSeptik_overallScore < 0.75 else "1"
+    saranaPembuanganBAB_class = "0" if saranaPembuanganBAB_overallScore < 0.95 else "1"
+    iplt_class = "0" if iplt_overallScore < 0.75 else "1"
+    saluranDrainase_class = "0" if saluranDrainase_overallScore < 0.5 else "1"
+    combined_class = tangkiSeptik_class + saranaPembuanganBAB_class + iplt_class + saluranDrainase_class
+    
+    row_data.append([row_id, tangkiSeptik_penggunaan, tangkiSeptik_terawat, tangkiSeptik_bauTidakSedap, tangkiSeptik_ketercemaranLingkungan, tangkiSeptik_jarakDariSumberAir,
+                    saranaPembuanganBAB_penggunaan, saranaPembuanganBAB_bangunanTerawat, saranaPembuanganBAB_adaDindingBangunan, saranaPembuanganBAB_adaVentilasiPintu, saranaPembuanganBAB_peneranganCukup, saranaPembuanganBAB_klosetLeherAngsa, saranaPembuanganBAB_adaAirBersih,
+                    iplt_penggunaan, iplt_bangunanTerawat, iplt_bauTidakSedap, iplt_pencemaranLingkungan, iplt_airLimbahBerwarna, iplt_jarakDariPermukiman,
+                    saluranDrainase_hierarki, saluranDrainase_jenis, saluranDrainase_bentukPenampang, saluranDrainase_perkerasanTepi, saluranDrainase_kondisiDrainase, saluranDrainase_bauTidakSedap, saluranDrainase_sedimentasiDrainase,
+                    combined_class])
+
+columns=["ID","Tangki Septik_Penggunaan","Tangki Septik_Terawat","Tangki Septik_Bau Tidak Sedap","Tangki Septik_Ketercemaran Lingkungan","Tangki Septik_Jarak Dari Sumber Air (m)",
+        "Sarana Pembuangan BAB_Penggunaan","Sarana Pembuangan BAB_Bangunan Terawat","Sarana Pembuangan BAB_Keberadaan Dinding Bangunan","Sarana Pembuangan BAB_Keberadaan Ventilasi Pintu","Sarana Pembuangan BAB_Penerangan yang Cukup","Sarana Pembuangan BAB_Terdapat Kloset Leher Angsa","Sarana Pembuangan BAB_Terdapat Sarana Air Bersih",
+        "IPLT_Penggunaan","IPLT_Bangunan Terawat","IPLT_Bau Tidak Sedap","IPLT_Pencemaran Lingkungan","IPLT_Air Limbah Berwarna","IPLT_Jarak Dari Permukiman (km)",
+        "Saluran Drainase_Hierarki Drainase","Saluran Drainase_Jenis Drainase","Saluran Drainase_Bentuk Penampang","Saluran Drainase_Perkerasan Tepi Drainase","Saluran Drainase_Kondisi Drainase","Saluran Drainase_Bau Tidak Sedap","Saluran Drainase_Sedimentasi Drainase",
+        "Target Class"]
+sni_df = pd.DataFrame(row_data, columns=columns)
+
+sni_df.to_csv("data/sni_df.csv", sep=",", index=False)
+print("Data created!")
